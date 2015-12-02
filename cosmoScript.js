b97202a9c6faca4c4b0d2cf4600c31d54a36f982
Simon Maennlein <maennlein@cip.ifi.lmu.de>
2015-12-02 18:53:15 +0100

    komische bezierkurven entstehen, wenn man auf einen punkt klickt...

--- cosmoScript.js
+++ cosmoScript.js
@@ -1,1 +1,16 @@
 
+
+
+function toggleClass(element, value){
+
+element.attr("class", function(index, classNames){
+
+        // toggle if element is selected or not
+        if(classNames.indexOf(value) > -1){
+            return classNames.replace(value, '');
+        } else {
+            return classNames + " "+value;
+        }
+        console.log($(area).attr("class"));
+    });
+}
